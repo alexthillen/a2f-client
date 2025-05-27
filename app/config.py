71b8141b75fd0db5a0856b77146d646e893a307a
Alex Thillen <alexthillen852@gmail.com>
2025-05-27 11:22:43 +0200

    Feat/init

---
+++ app/config.py
@@ -0,0 +1,46 @@
+import os
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Config:
+    """Application configuration settings."""
+
+    TITLE = os.getenv("A2F_FASTAPI_TITLE", "A2F Streaming Service API")
+    DESCRIPTION = os.getenv("A2F_FASTAPI_DESCRIPTION", "An API for streaming blendshapes.")
+    VERSION = os.getenv("A2F_FASTAPI_VERSION", "0.1.0")
+    HOST = os.getenv("A2F_FASTAPI_HOST", "0.0.0.0")
+    PORT = int(os.getenv("A2F_FASTAPI_PORT", "8000"))
+    DEFAULT_WORKERS = int(os.getenv("A2F_FASTAPI_DEFAULT_WORKERS", "2"))
+    CLIENTS_PER_WORKER = int(os.getenv("A2F_FASTAPI_CLIENTS_PER_WORKER", "2"))
+    TMP_DIR = os.getenv("A2F_FASTAPI_TMP_DIR", "tmp")
+    LOG_FILE = os.getenv("A2F_FASTAPI_LOG_FILE", "a2fserver.log")
+
+
+class EmotionWeights(BaseModel):
+    amazement: float = 0.0
+    anger: float = 0.0
+    cheekiness: float = 0.0
+    disgust: float = 0.0
+    fear: float = 0.0
+    grief: float = 0.0
+    joy: float = 0.0
+    outofbreath: float = 0.0
+    pain: float = 0.0
+    sadness: float = 0.0
+
+    def __dict__(self) -> dict:
+        """Convert the EmotionWeights model to a dictionary."""
+        return {
+            "amazement": self.amazement,
+            "anger": self.anger,
+            "cheekiness": self.cheekiness,
+            "disgust": self.disgust,
+            "fear": self.fear,
+            "grief": self.grief,
+            "joy": self.joy,
+            "outofbreath": self.outofbreath,
+            "pain": self.pain,
+            "sadness": self.sadness,
+        }
---
+++ app/streaming_logic.py
@@ -0,0 +1,166 @@
+import asyncio
+import json
+import math
+import os
+import platform
+import random
+import time
+from typing import Optional, Union
+
+from loguru import logger
+
+from a2f_client import A2FClient
+from a2f_client.utils import load_audio
+from app.config import Config, EmotionWeights
+
+if platform.system() == "Windows":
+    import msvcrt
+else:
+    import fcntl
+
+
+class StreamingManager:
+    def __init__(self, clients_per_worker: int = 2):
+        self.available_ports = [
+            i for i in range(8190, 8190 + Config.DEFAULT_WORKERS * clients_per_worker)
+        ]
+        self.locked_files = {}
+        self.a2f_clients = [
+            A2FClient(port=str(self._next_port())) for _ in range(clients_per_worker)
+        ]
+        self.client_locks = [asyncio.Lock() for _ in self.a2f_clients]
+        self.chunk_size = 0.3
+        self.current_fps = 30
+        self.default_fps = 10
+        self.max_fps = 30
+        self.min_fps = 10
+
+    def _acquire_lock(self, number):
+        lock_file_path = f".lock_{number}"
+        lock_file = None
+        try:
+            lock_file = open(lock_file_path, "w")
+            if platform.system() == "Windows":
+                msvcrt.locking(lock_file.fileno(), msvcrt.LK_NBLCK, 1)
+            else:
+                fcntl.flock(lock_file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+            print(f"Process {os.getpid()} acquired lock on {lock_file_path}")
+            self.locked_files[number] = lock_file
+            return True
+        except FileNotFoundError:
+            try:
+                lock_file = open(lock_file_path, "w")
+                if platform.system() == "Windows":
+                    msvcrt.locking(lock_file.fileno(), msvcrt.LK_NBLCK, 1)
+                else:
+                    fcntl.flock(lock_file.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                print(f"Process {os.getpid()} created and acquired lock on {lock_file_path}")
+                self.locked_files[number] = lock_file
+                return True
+            except (BlockingIOError, OSError):
+                if lock_file:
+                    lock_file.close()
+                return False
+        except (BlockingIOError, OSError):
+            if lock_file:
+                lock_file.close()
+            return False
+
+    def _release_lock(self, number):
+        lock_file_path = f".lock_{number}"
+        if number in self.locked_files:
+            lock_file = self.locked_files[number]
+            try:
+                if platform.system() == "Windows":
+                    msvcrt.locking(lock_file.fileno(), msvcrt.LK_UNLCK, 1)
+                else:
+                    fcntl.flock(lock_file.fileno(), fcntl.LOCK_UN)
+            except OSError as e:
+                print(f"Error unlocking file {lock_file_path}: {e}")
+            finally:
+                lock_file.close()
+                del self.locked_files[number]
+                print(f"Process {os.getpid()} released lock on {lock_file_path}")
+        else:
+            print(f"No lock found for {lock_file_path}")
+
+    def _next_port(self):
+        for _ in range(10):
+            for port in self.available_ports:
+                time.sleep(random.uniform(0.1, 0.5))
+                if self._acquire_lock(port):
+                    return port
+        raise RuntimeError(f"Process {os.getpid()} : No available ports found after 10 attempts.")
+
+    def __del__(self):
+        print(f"Releasing all locks for process {os.getpid()}")
+        for port in list(self.locked_files.keys()):
+            self._release_lock(port)
+        for lock in self.client_locks:
+            lock.release()
+        print(f"All locks released for process {os.getpid()}")
+
+    async def generate_blendshapes_coroutine(self, client_idx, start, end, use_a2e) -> dict:
+        """Offload blocking `.generate_blendshapes` call to asyncio's default ThreadPoolExecutor."""
+        async with self.client_locks[client_idx]:
+            print(f"→ scheduling blocking export on client {client_idx}, {start:.1f}-{end:.1f}s")
+            client: A2FClient = self.a2f_clients[client_idx]
+            loop = asyncio.get_running_loop()
+            start_time = time.time()
+            result = await loop.run_in_executor(
+                None,
+                func=lambda: client.generate_blendshapes(start, end, self.current_fps, use_a2e)[
+                    "blendshapes"
+                ],
+            )
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            alpha = (end - start) / elapsed_time
+            safe_fps = max(
+                self.min_fps, math.floor(len(self.a2f_clients) * self.current_fps * alpha - 7)
+            )
+            self.current_fps = min(self.max_fps, (self.current_fps + safe_fps) // 2)
+            logger.info(
+                f"Adjusted FPS to {self.current_fps} as generation took {elapsed_time:.2f}s."
+            )
+
+            return result
+
+    async def submit_job(
+        self,
+        audio_source: str,
+        fps: Optional[int] = None,
+        emotions: Optional[EmotionWeights] = None,
+        use_a2e: bool = False,
+    ):
+        if fps is None:
+            fps = self.default_fps
+        self.current_fps = fps
+        audio_path, duration = load_audio(audio_source)
+        for client in self.a2f_clients:
+            client.set_audio(audio_path)
+            if emotions:
+                client.set_emotions(dict(emotions))
+
+        nr_splits = math.ceil(duration / self.chunk_size - 0.5)
+        # Fire off ALL tasks immediately
+        tasks = []
+
+        chunks = [
+            (i * self.chunk_size, min((i + 1) * self.chunk_size, duration))
+            for i in range(nr_splits)
+        ]
+        for i, (start, end) in enumerate(chunks):
+            client_idx = i % len(self.a2f_clients)
+            task = asyncio.create_task(
+                self.generate_blendshapes_coroutine(client_idx, start, end, use_a2e)
+            )
+            print(f"Created task #{i} on client {i % len(self.a2f_clients)}")
+            tasks.append(task)
+
+        # Await tasks one-by-one in order!
+        for idx, task in enumerate(tasks):
+            result = await task
+            print(f"Process {os.getpid()} :Chunk #{idx} done")
+            json_str = json.dumps({"chunk_id": idx, "result": result}).encode("utf-8") + b"\n"
+            yield json_str
